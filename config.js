ca7ad87f61baecb3309765f5e23208cd8f9a5e32
gooeybea <gooeybea@gmail.com>
2025-09-03 11:51:28 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,36 +1,54 @@
 // Username displayed in greeting.
 var userName = "gabeweb";
 
+// Default Searchengine
+// Available: Google DuckDuckGo Bing Yahoo
+var searchEngine = "DuckDuckGo";
+
 // Cards
 var cards = [
-  {
-    name: "General",
-    bookmarks: {
-      "Aol Mail": "https://mail.aol.com",
-    },
-  },
-  {
-    name: "News",
-    bookmarks: {
-      "9to5Google": "https://9to5google.com",
-    },
-  },
-  {
-    name: "Social",
-    bookmarks: {
-      Bluesky: "https://bsky.app/",
-    },
-  },
-  {
-    name: "Media",
-    bookmarks: {
-      Behance: "https://behance.net",
-    },
-  },
-  {
-    name: "Tools",
-    bookmarks: {
-      "Edit Start Page Links": "https://github.com/gooeybea/startpage/edit/main/config.js",
-    },
-  },
-];
+	{
+		name: "Email",
+		bookmarks: {
+			"Aol Mail": "https://mail.aol.com",
+			"Google Mail": "https://mail.google.com",
+			"OnMail": "https://mail.onmail.com",
+			"Outlook": "https://mail.live.com",
+			"Proton Mail": "https://mail.proton.me",
+			"Yahoo Mail": "https://mail.yahoo.com",
+			"Yandex Mail": "https://mail.yandex.com"
+		}
+	},
+	{
+		name: "News",
+		bookmarks: {
+			"Digg": "https://digg.com",
+			"Quora": "https://quora.com",
+			"Reddit": "https://reddit.com",
+			"Stack Exchange": "https://stackexchange.com"
+		}
+	},
+	{
+		name: "Social",
+		bookmarks: {
+			"Discord": "https://discordapp.com",
+			"Facebook": "https://facebook.com",
+			"Instagram": "https://instagram.com",
+			"Tumblr": "https://tumblr.com",
+			"Twitter": "https://twitter.com",
+			"VK": "https://vk.com"
+		}
+	},
+	{
+		name: "Media",
+		bookmarks: {
+			"Crumchyroll": "https://crunchyroll.com",
+			"Last.fm": "https://last.fm",
+			"Pluto TV": "https://pluto.tv",
+			"Runtime": "https://runtime.tv",
+			"Spotify": "https://open.spotify.com",
+			"Trakt.tv": "https://trakt.tv",
+			"YouTube": "https://youtube.com"
+		}
+	}
+]
